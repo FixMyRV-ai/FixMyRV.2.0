0cd73ff8e35005b31f9cf1f9d80da38e7a33359d
TheRiver <jorgerioscantu@hotmail.com>
2025-09-04 22:01:39 -0600

    Force Railway Node.js deployment - remove all HTML files

--- temp-frontend/src/lib/stripe.ts
+++
@@ -1,8 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-// Replace with your Stripe publishable key
-const stripePromise = loadStripe(
-  "pk_test_51QSvjmCIIdlgrt8vYQDgIcXjMFdbw4GveGBT6ob15ICaLCBOnsWETojtwtIm4VGCt34GIZRq3D7TW29mlRZGKF1Q00c4KwqmnQ"
-);
-
-export default stripePromise;
