4ff6267b565bceab96d8b907c44700a3c90412a3
Austin Kettner <austinkettner@users.noreply.github.com>
2025-09-23 18:19:01 -0400

    Cleanup

--- delete_user.sql
+++
@@ -1,8 +0,0 @@
-\echo 'Searching for user with email jorgerioscantu@hotmail.com...'
-SELECT id, email, "firstName", "lastName", "organizationId" FROM organization_users WHERE email = 'jorgerioscantu@hotmail.com';
-
-\echo 'Deleting user...'
-DELETE FROM organization_users WHERE email = 'jorgerioscantu@hotmail.com';
-
-\echo 'Verifying deletion...'
-SELECT id, email, "firstName", "lastName", "organizationId" FROM organization_users WHERE email = 'jorgerioscantu@hotmail.com';
